789606e770fa05ca23d4eb21346d13b2186a5309
unknown <jayadeeppasupuleti7731@gmail.com>
2026-02-02 14:33:13 +0530

    final commit

--- src/main/java/com/employee/entity/EmpProfileView.java
+++ src/main/java/com/employee/entity/EmpProfileView.java
@@ -12,73 +12,79 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="sce_emp_profile",schema="sce_employee")
+@Table(name = "sce_emp_profile", schema = "sce_employee")
 public class EmpProfileView {
-	  @Id
-	    @Column(name = "emp_id")
-	    private Integer empId;
+	@Id
+	@Column(name = "emp_id")
+	private Integer empId;
 
-	    @Column(name = "full_name")
-	    private String fullName;
+	@Column(name = "full_name")
+	private String fullName;
 
-	    @Column(name = "cmps_id")
-	    private Integer campusId;
+	@Column(name = "cmps_id")
+	private Integer campusId;
 
-	    @Column(name = "cmps_name")
-	    private String campusName;
+	@Column(name = "cmps_name")
+	private String campusName;
 
-	    @Column(name = "date_of_birth")
-	    private String dateOfBirth;
+	@Column(name = "date_of_birth")
+	private String dateOfBirth;
 
-	    @Column(name = "gender_name")
-	    private String genderName;
+	@Column(name = "gender_name")
+	private String genderName;
 
-	    @Column(name = "marital_status_type")
-	    private String maritalStatusType;
+	@Column(name = "marital_status_type")
+	private String maritalStatusType;
 
-	    @Column(name = "temp_payroll_id")
-	    private String tempPayrollId;
+	@Column(name = "temp_payroll_id")
+	private String tempPayrollId;
 
-	    @Column(name = "payroll_id")
-	    private String payrollId;
+	@Column(name = "payroll_id")
+	private String payrollId;
 
-	    @Column(name = "payroll_company_id")
-	    private Integer payrollCompanyId;
+	@Column(name = "payroll_company_id")
+	private Integer payrollCompanyId;
 
-	    @Column(name = "payroll_company")
-	    private String payrollCompany;
+	@Column(name = "payroll_company")
+	private String payrollCompany;
 
-	    @Column(name = "department_name")
-	    private String departmentName;
+	@Column(name = "department_name")
+	private String departmentName;
 
-	    @Column(name = "designation_name")
-	    private String designationName;
+	@Column(name = "designation_name")
+	private String designationName;
 
-	    @Column(name = "primary_mobile_no")
-	    private String primaryMobileNo;
+	@Column(name = "primary_mobile_no")
+	private String primaryMobileNo;
 
-	    @Column(name = "email")
-	    private String email;
+	@Column(name = "email")
+	private String email;
 
-	    @Column(name = "reporting_manager_id")
-	    private Integer reportingManagerId;
+	@Column(name = "reporting_manager_id")
+	private Integer reportingManagerId;
 
-	    @Column(name = "reporting_manager_name")
-	    private String reportingManagerName;
+	@Column(name = "reporting_manager_name")
+	private String reportingManagerName;
 
-	    @Column(name = "blood_group_name")
-	    private String bloodGroupName;
+	@Column(name = "blood_group_name")
+	private String bloodGroupName;
 
-	    @Column(name = "contract_start_date")
-	    private String contractStartDate;
+	@Column(name = "contract_start_date")
+	private String contractStartDate;
 
-	    @Column(name = "contract_end_date")
-	    private String contractEndDate;
+	@Column(name = "contract_end_date")
+	private String contractEndDate;
 
-	    @Column(name = "contract_period")
-	    private Double contractPeriod;
+	@Column(name = "contract_period")
+	private Double contractPeriod;
 
-	    @Column(name = "emp_type")
-	    private String empType;
+	@Column(name = "emp_type")
+	private String empType;
+
+	@Column(name = "mode_of_hiring_id")
+	private Integer modeOfHiringId;
+
+	@Column(name = "mode_of_hiring_name")
+	private String modeOfHiringName;
 
 }
--- src/main/java/com/employee/entity/EmployeeOnboardingView.java
+++ src/main/java/com/employee/entity/EmployeeOnboardingView.java
@@ -1,5 +1,5 @@
 package com.employee.entity;
- 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -7,7 +7,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
- 
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,83 +17,88 @@ public class EmployeeOnboardingView {
     @Id
     @Column(name = "emp_id")
     private Integer empId;
- 
+
     @Column(name = "emp_name")
     private String fullName;
- 
+
     @Column(name = "date_of_birth")
     private String dateOfBirth;
- 
+
     @Column(name = "gender_id")
     private Integer genderId;
- 
+
     @Column(name = "gender_name")
     private String genderName;
- 
+
     @Column(name = "designation_id")
     private Integer designationId;
- 
+
     @Column(name = "designation_name")
     private String designationName;
- 
+
     @Column(name = "temp_payroll_id")
     private String tempPayrollId;
- 
+
     @Column(name = "payroll_id")
     private String payrollId;
- 
+
     @Column(name = "primary_mobile_no")
     private String primaryMobileNo;
- 
+
     @Column(name = "email")
     private String email;
- 
+
     @Column(name = "adhaar_no")
     private String adhaarNo;
- 
+
     @Column(name = "adhaar_enrolment_no")
     private String adhaarEnrolmentNo;
- 
+
     @Column(name = "pancard_no")
     private String pancardNo;
- 
+
     @Column(name = "ssc_no")
     private String sscNo;
- 
+
     @Column(name = "category_id")
     private Integer categoryId;
- 
+
     @Column(name = "category_name")
     private String categoryName;
- 
+
     @Column(name = "pre_esi_no")
     private String preEsiNo;
- 
+
     @Column(name = "uan_no")
     private String uanNo;
- 
+
     @Column(name = "personal_email")
     private String personalEmail;
- 
+
     @Column(name = "blood_group_id")
     private Integer bloodGroupId;
- 
+
     @Column(name = "blood_group_name")
     private String bloodGroupName;
- 
+
     @Column(name = "total_experience")
     private Double totalExperience;
- 
+
     @Column(name = "highest_qualification_id")
     private Integer highestQualificationId;
- 
+
     @Column(name = "highest_qualification")
     private String highestQualification;
- 
+
+    @Column(name = "mode_of_hiring_id")
+    private Integer modeOfHiringId;
+
+    @Column(name = "mode_of_hiring_name")
+    private String modeOfHiringName;
+
     @Column(name = "emp_type")
     private String empType;
- 
+
     @Column(name = "emp_level_id")
     private String empLevelId;
 }
- 
