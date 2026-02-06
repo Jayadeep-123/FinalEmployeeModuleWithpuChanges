a9852e1f3d45e0e19571c2a1e1b70e83b6c5b110
unknown <jayadeeppasupuleti7731@gmail.com>
2026-02-06 18:07:24 +0530

    Dropdown updated

--- src/main/java/com/employee/entity/EmpOnboardingStatusView.java
+++ src/main/java/com/employee/entity/EmpOnboardingStatusView.java
@@ -16,21 +16,22 @@ import lombok.NoArgsConstructor;
 @Table(name = "emp_onboarding_status", schema = "sce_employee")
 public class EmpOnboardingStatusView {
 
-	@Id
-	private int emp_id;
+	private Integer emp_id;
 	private String employee_name;
-	private int cmps_id;
+	private Integer cmps_id;
 	private String cmps_name;
 	private Integer category_id;
 	private String category_name;
 	private String payroll_id;
+	@Id
 	private String temp_payroll_id;
 	private Date date_of_join;
 	private Date leaving_date;
+	private Integer gender_id;
 	private String gender_name;
 	private String city_name;
 	private String remarks;
-	private int join_type_id;
+	private Integer join_type_id;
 	private String join_type;
 	private String check_app_status_name;
 
--- src/main/java/com/employee/repository/EmpOnboardingStatusViewRepository.java
+++ src/main/java/com/employee/repository/EmpOnboardingStatusViewRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.employee.entity.EmpOnboardingStatusView;
 
 @Repository
-public interface EmpOnboardingStatusViewRepository extends JpaRepository<EmpOnboardingStatusView, Integer> {
+public interface EmpOnboardingStatusViewRepository extends JpaRepository<EmpOnboardingStatusView, String> {
 
     @Query("SELECT e FROM EmpOnboardingStatusView e WHERE UPPER(e.category_name) = UPPER(:categoryName)")
     List<EmpOnboardingStatusView> findByCategoryName(@Param("categoryName") String categoryName);
