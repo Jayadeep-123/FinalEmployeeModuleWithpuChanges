ee1f0be0a156e853be5373428ad2993e8b402701
unknown <jayadeeppasupuleti7731@gmail.com>
2026-02-25 12:32:53 +0530

    updated one

--- src/main/java/com/employee/config/CorsConfig.java
+++ src/main/java/com/employee/config/CorsConfig.java
@@ -1,28 +1,31 @@
 package com.employee.config;
-
+ 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
+ 
 import java.util.Arrays;
-
+ 
 @Configuration
 public class CorsConfig {
-
+ 
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000"); 
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://192.168.20.19:8081");
+        config.addAllowedOrigin("http://192.168.20.14:8081");
         config.addAllowedHeader("*");
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setMaxAge(3600L);
-
+ 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 }
+ 
